4b0e60e594fbf5dda57ca2cf91683b30331cbfce
Danilo Olivera <sdanilo109@gmail.com>
2020-12-11 21:31:40 -0300

    Validando nome, email e colocando perfumaria

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
+# Dependencias 
+# pip3 install psycopg2
+# pip3 install tabulate
+# pip3 install validate-email
+
 import psycopg2
 import time
 import re
+import pandas as pd
+from validate_email import validate_email
 
 #***************************************************************************#
 
@@ -14,33 +21,14 @@ def conexao():
         database = "dan"
     )
     return con
-    
-    
+
+   
 #***************************************************************************#
 class Users():
     def __init__(self, nome, email):
         self.nomeUser = nome
         self.emailUser = email
-        
-#***************************************************************************#
-    def selecaoEmail(self):
-        con = conexao()
-        cursor = con.cursor()
-        cursor.execute(f"select nome, email from users where email = '{self.emailUser}'")
-        result = cursor.fetchall()
-        for row in result:
-            print(row[0], ' | ', row[1])
-        con.commit()
-        con.close()
- 
-       
-#***************************************************************************#
-    def InserirAluno(self):
-        con = conexao()
-        cursor = con.cursor()
-        cursor.execute(f"insert into users (nome, email) values ('{self.nomeUser}', '{self.emailUser}')")
-        con.commit()
-        con.close()
+
         
 #***************************************************************************#
     def AlterarInformacao(self, emailAtual):
@@ -56,77 +44,118 @@ class Users():
         cursor.execute(exeSQL)
         con.commit()
         con.close()
+ 
         
 #***************************************************************************#
-    def DeletarInformacao(self):
-        con = conexao()
-        cursor = con.cursor() 
-        cursor.execute(f"delete from users where email = '{self.emailUser}'")
-        con.commit()
-        con.close()
+def InserirAluno(novoUser, novoEmail):
+    con = conexao()
+    cursor = con.cursor()
+    cursor.execute(f"insert into users (nome, email) values ('{novoUser}', '{novoEmail}')")
+    con.commit()
+    con.close()   
+    
+  
+#***************************************************************************#
+def deleteUser(excluirUser):
+    con = conexao()
+    cursor = con.cursor() 
+    cursor.execute(f"delete from users where email = '{excluirUser}'")
+    con.commit()
+    con.close()
         
 
 #***************************************************************************#
 def selecaoTudo():
+    column_names = ["Nome do Usuário", "E-mail"]
     con = conexao()
     cursor = con.cursor()
-    cursor.execute("select * from users")
+    cursor.execute("select nome, email from users")
     result = cursor.fetchall()
-    for row in result:
-        print(row[1], ' | ', row[2])
     con.commit()
     con.close()
+    
+    df = pd.DataFrame(result, columns=column_names)
+    df.set_index('Nome do Usuário', inplace=True)
+    return print(df.to_markdown())
 
 
 #***************************************************************************#
-def verifcadorTabela(e):
+def verifcadorUser(nomeVerificado, emailVerificado):
+    column_names = ["Nome do Usuário", "E-mail"]
+    con = conexao()
+    cursor = con.cursor()
+    exeSQL = f"select nome, email from users where nome = '{nomeVerificado}' and email = '{emailVerificado}'"
+    cursor.execute(exeSQL)
+    result = cursor.fetchall()
+    result.append("")
+
+    if f"{emailVerificado}" in result[0]:
+        print("\033[36mEsse o seu cadastro atual\033[m\n")
+        df = pd.DataFrame(result, columns=column_names)
+        df.set_index('Nome do Usuário', inplace=True)
+        print(df.to_markdown())
+            
+        return True
+   
+    else:
+        print("\033[31mO usuario não está cadastrado. Por favor cadastra-se\033[m")
+        return False
+ 
+    con.commit()
+    con.close
+
+#***************************************************************************#
+def verifcadorEmail(e):
+    column_names = ["Nome do Usuário", "E-mail"]
     con = conexao()
     cursor = con.cursor()
     exeSQL = f"select nome, email from users where email = '{e}'"
     print(exeSQL)
     cursor.execute(exeSQL)
     result = cursor.fetchall()
-    try:
-        if f"{e}" in result[0]:
-            print("Esse o seu cadastro atual")
-            linha()
-            for row in result:
-                print(row[0], ' | ', row[1])
-                
-            return True
-    except:
-            linha()
-            print("O usuario não está cadastrado. Por favor cadastra-se")
-            linha()
+    result.append("")
+
+    if f"{e}" in result[0]:
+        print("\033[36mEsse o seu cadastro atual\033[m\n")
+        df = pd.DataFrame(result, columns=column_names)
+        df.set_index('Nome do Usuário', inplace=True)
+        print(df.to_markdown())
             
-            return False
+        return True
+   
+    else:
+        print("\033[31mO usuario não está cadastrado. Por favor cadastra-se\033[m")
+        
+        return False
         
     con.commit()
     con.close
- 
-
+    
+    
 #***************************************************************************#
-regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
 def validadorNomeEmail(validadorNome, validadorEmail): 
 
-    regex_name = re.compile(r'[A-Z][a-z].* [A-Z][a-z].*', 
+    regex_name = re.compile(r'^([A-Za-z]+)( [A-Za-z]+)*( [A-Za-z]+)*$', 
             re.IGNORECASE) 
 
     res = regex_name.search(validadorNome)
     
+    validandoEmail = validate_email(validadorEmail)
+    
+    if validandoEmail == True: 
+        return validandoEmail
+
+    else: 
+        print("\033[31mInvalid Email\033[m")
+        return False
+ 
     if res:
         return True
-        
+  
     else:
-        linha()
-        print("Seu nome está inválido")
+        print("\033[31mSeu nome está inválido\033[m")
+        return False
  
-    if(re.search(regex,validadorEmail)):
-        return True
-        
-    else:
-        linha()
-        print("Seu email está inválido")
 
 
 #***************************************************************************#
@@ -145,91 +174,110 @@ def linha():
 
 #***************************************************************************#
 def tela(opcao):
-    print("Aguarde...")
+    print("\033[36mAguarde...\033[m")
     time.sleep(0.8)
     linha()
     if opcao == '1':
         resposta = 'S'
-        nomeC = str(input("Informe o seu Nome Completo: ")).strip().capitalize()
-        emailC = input("Digite seu email: ").strip()
+        nomeC = str(input("\033[33mInforme o seu Nome Completo\033[m: ")).strip().capitalize()
+        emailC = input("\033[33mDigite seu email\033[m: ").strip()
         validador = validadorNomeEmail(nomeC, emailC)
         if len(nomeC) > 2 and len(emailC) > 2 and validador == True:
-            user = Users(nomeC, emailC)
-            user.InserirAluno()
-            print("Aguarde...")
+            #user = Users(nomeC, emailC)
+            newUser = InserirAluno(nomeC, emailC)
+            print("\033[36mAguarde...\033[m")
             time.sleep(2)
             linha()
-            print("Usuario Cadastrado com sucesso\n")
+            print("\033[32mUsuario Cadastrado com sucesso\033[m\n")
         else:
             linha()
             time.sleep(1)
-            print("Suas credencial estão erradas, por favor verifique seu Nome ou E-mail!!\n")
+            print("\033[31mSuas credencial estão erradas, por favor verifique seu Nome ou E-mail!!\033[m\n")
             
 
     if opcao == '2':
-        emailAtual = input("Digite o seu E-mail atual: ")
-        nomeAtual = str(input("Digite o seu Nome atual: "))
-        ver = verifcadorTabela(emailAtual)
+        emailAtual = input("\033[33mDigite o seu E-mail atual\033[m: ").strip()
+        nomeAtual = str(input("\033[33mDigite o seu Nome atual\033[m: ")).strip().capitalize()
+        ver = verifcadorUser(nomeAtual, emailAtual)
         linha()
-        mudar = str(input("Gostaria de mudar o seu cadastro (s/n)? ")).strip().upper()[0]
-        while mudar not in 'SN':
-            mudar = str(input('Dados Inválidos. Por favor, informe com S ou N: ')).strip().upper()[0]
-        if mudar == 'S' and ver == True:
+        if ver == True:
+            mudar = str(input("\033[36mGostaria de mudar o seu cadastro (s/n)?\033[m ")).strip().upper()[0]
+            while mudar not in 'SN':
+                mudar = str(input('\033[31mDados Inválidos. Por favor, informe com S ou N\033[m: ')).strip().upper()[0]
+            if mudar == 'S' and ver == True:
+                    
+                print('\033[36mFazendo Login\033[m')
+                print('\033[36mAguarde...\033[m')
+                time.sleep(2)
+                linha()
+                print('''
+    [ 1 ] Para trocar o Usuário e senha.
+    [ 2 ] Para trocar Usuário.
+    [ 3 ] Para trocar Email.
+    [ 4 ] Sair.
+                    ''')
+                op = int(input('\033[36mQual e a Opçâo\033[m: '))
+                print('\033[36mProcessando ...\033[m')
+                time.sleep(0.6)
                 
-            print('Fazendo Login')
-            print('Aguarde...')
-            time.sleep(2)
-            linha()
-            print('''
-[ 1 ] Para trocar o Usuário e senha.
-[ 2 ] Para trocar Usuário.
-[ 3 ] Para trocar Senha.
-[ 4 ] Sair.
-                ''')
-            op = int(input('Qual e a Opçâo: '))
-            print('Processando ...')
-            time.sleep(0.6)
-            
-            if op == 1:
-                emailAtual = input("Digite o seu E-mail para identificá-lo: ")
-                nomeNovo = str(input("Digite o seu novo nome: "))
-                emailNovo = input("Digite o seu novo email: ")
-                user = Users(nomeNovo, emailNovo)
-                user.AlterarInformacao(emailAtual)
-                linha()
-                print("Usuario Alterado com sucesso")
-                linha()
+                if op == 1:
+                    emailAtual = input("\033[33mDigite o seu E-mail para identificá-lo\033[m: ").strip()
+                    nomeNovo = str(input("\033[33mDigite o seu novo nome\033[m: ")).strip().capitalize()
+                    emailNovo = input("\033[33mDigite o seu novo email\033[m: ").strip()
+                    validador = validadorNomeEmail(nomeNovo, emailNovo)
+                    if len(nomeNovo) > 2 and len(emailNovo) > 2 and validador == True:
+                        user = Users(nomeNovo, emailNovo)
+                        user.AlterarInformacao(emailAtual)
+                        linha()
+                        print("\033[32mUsuario Alterado com sucesso\033[m")
+                        linha()
+                    else:
+                        linha()
+                        time.sleep(1)
+                        print("\033[31mSuas credencial estão erradas, por favor verifique seu Nome ou E-mail!!\033[m\n")
+                            
+                elif op == 2:
+                    emailAtual = input("\033[33mDigite o seu E-mail para identificá-lo\033[m: ").strip()
+                    nomeNovo = str(input("\033[33mDigite o seu novo nome\033[m: ")).strip().capitalize()
+                    validador = validadorNomeEmail(nomeNovo, emailAtual)
+                    if len(nomeNovo) > 2 and len(emailAtual) > 2 and validador == True:
+                        user = Users(nomeNovo, emailAtual)
+                        user.AlterarInformacao(emailAtual)
+                        linha()
+                        print("\033[32mNome Alterado com sucesso\033[m")
+                        linha()
+                    else:
+                        linha()
+                        time.sleep(1)
+                        print("\033[31mSuas credencial estão erradas, por favor verifique seu Nome ou E-mail!!\033[m\n")
+                        
+                elif op == 3:
+                    emailAtual = input("\033[33mDigite o seu E-mail para identificá-lo\033[m: ").strip()
+                    emailNovo = str(input("\033[33mDigite o seu novo email\033[m: ")).strip()
+                    validador = validadorNomeEmail(emailNovo, emailAtual)
+                    if len(emailNovo) > 2 and len(emailAtual) > 2 and validador == True:
+                        user = Users(emailNovo, emailAtual)
+                        user.AlterarInformacao(emailAtual)
+                        linha()
+                        print("\033[32mE-mail Alterado com sucesso\033[m")
+                        linha()
+                    else:
+                        linha()
+                        time.sleep(1)
+                        print("\033[31mSuas credencial estão erradas, por favor verifique seu Nome ou E-mail!!\033[m\n")
+                elif op == 4:
+                    linha()
+                    print("\033[32mCerto, Obrigado!!\033[m")
+                    linha()
                 
-            elif op == 2:
-                emailAtual = input("Digite o seu E-mail para identificá-lo: ")
-                nomeNovo = str(input("Digite o seu novo nome: "))
-                user = Users(nomeNovo, emailAtual)
-                user.AlterarInformacao(emailAtual)
+                elif op != 1 or op != 2 or op != 3 or op != 4:
+                    linha()
+                    print("\033[31mEssa opção não existe, por favor informe corretamente\033[m")
+                    linha()
+            else:
                 linha()
-                print("Nome Alterado com sucesso")
+                print("\033[32mCerto, Obrigado!!\033[m")
                 linha()
-                
-            elif op == 3:
-                emailAtual = input("Digite o seu E-mail para identificá-lo: ")
-                emailNovo = str(input("Digite o seu novo email: "))
-                user = Users(nomeAtual, emailNovo)
-                user.AlterarInformacao(emailAtual)
-                linha()
-                print("E-mail Alterado com sucesso")
-                linha()
-            
-            elif op == 4:
-                linha()
-                print("Certo, Obrigado!!")
-            
-            elif op != 1 or op != 2 or op != 3 or op != 4:
-                linha()
-                print("Essa opção não existe, por favor informe corretamente")
-                linha()
-        else:
-            linha()
-            print("Certo, Obrigado!!")
-            linha()
             
             
 #***************************************************************************#
@@ -242,58 +290,71 @@ while opcao != 6:
 [ 4 ] Listar E-mail Cadastrado.
 [ 5 ] Finaliza o Programa.''')
     linha()
-    opcao = input("Qual e a opção: ")
+    opcao = input("\033[36mQual e a opção\033[m: ")
     linha()
     while opcao not in '1' '2' '3' '4' '5' '6':
-        opcao = input("Por favor digite corretamente: ")
+        opcao = input("\033[31mPor favor digite corretamente: ")
         linha()
-    # Opção "1"
-    if opcao == '1':
-        tela(opcao)
-        titulo()
+    try:
+        # Opção "1"
+        if opcao == '1':
+            tela(opcao)
+            titulo()
+            
+
         
-
-    
-# Opção "2"
-    elif opcao == '2':
-        tela(opcao)
+    # Opção "2"
+        elif opcao == '2':
+            tela(opcao)
 
 
-    
-# Opção "3"
-    elif opcao == '3':
-        user = Users("","")
-        user.selecaoEmail()
-        email = input("Informe o email para deletar: ")
-        user = Users("",email)
-        if (input("Tem certeza que deseja excluir esse usuário? Essa ação não poderá ser desfeita (s/n): ") == "s"):
-            user.DeletarInformacao()
+        
+    # Opção "3"
+        elif opcao == '3':
+            emailExcluir = input("\033[33mDigite o seu E-mail atual\033[m: ").strip()
             linha()
-            print("Usuario excluido com sucesso")
-        else:
+            verificador = verifcadorEmail(emailExcluir)
+            #userExcluir = deleteUser(emailExcluir)
+            if verificador == True:
+                linha()
+                res = str(input("\033[31mTem certeza que deseja excluir esse usuário? Essa ação não poderá ser desfeita (s/n)\033[m: ")).strip().upper()[0]
+                while res not in 'SN':
+                    res = str(input('\033[31mDados Inválidos. Por favor, informe com S ou N\033[m: ')).strip().upper()[0]
+                if res == 'S':
+                    linha()
+                    excluirUser = deleteUser(emailExcluir)
+                    print("\033[32mUsuario excluido com sucesso\033[m")
+                else:
+                    linha()
+                    print("\033[32mCerto, Obrigado\033[m")
+            elif verificador == False:
+                linha()
+                print("\033[31mNão há nada para excluir\033[m")
             linha()
-            print("Não há nada para excluir")
-        linha()
 
+        
+    # Opção "4"
+        elif opcao == '4':
+            selecionarPorEmail = input("\033[33mInforme o seu E-mail\033[m: ").strip()
+            user = verifcadorEmail(selecionarPorEmail)
+            print("")
+    # Opção "5"
+        elif opcao == '5':
+            print("\033[36mProcessando Todos os Dados\033[m")
+            time.sleep(2)
+            linha()
+            print("\033[36mTodos os cadastrados\033[m")
+            linha()
+            selecaoTudo()
+            linha()
+            break
+    except ValueError:
+        print("\033[31mDigite corretamente\033[m")
+        linha()
     
-# Opção "4"
-    elif opcao == '4':
-        selecionarPorEmail = input("Informe o seu E-mail: ")
-        user = Users("",selecionarPorEmail)
-        linha()
-        user.selecaoEmail()
+    except KeyboardInterrupt:
+        print("\033[31mDigite corretamente\033[m")
         linha()
 
-
-# Opção "5"
-    elif opcao == '5':
-        print("Processando Todos os Dados")
-        time.sleep(2)
-        linha()
-        print("Todos os cadastrados")
-        linha()
-        selecaoTudo()
-        linha()
-        break
-print('Fim do Programa! Obrigado!!')
-print("")
+print('\033[35mFim do Programa! Obrigado!!\033[m')
+        
